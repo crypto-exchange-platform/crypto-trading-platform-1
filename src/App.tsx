cde1293743773b338efde2544d06c5d2cd409096
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2025-06-11 18:28:51 +0400

    chore: setup project with Vite, React, Tailwind, ShadCN UI

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+            <h1 className="text-4xl font-bold mb-4">Crypto Landing</h1>
+        </div>
+    );
+}
+
+export default App;
